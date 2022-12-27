17269bdbb0bc883a8ea0456575f1cc8fd02c75ac
Boudjada Yasser <by.root96@gmail.com>
2022-12-27 09:14:40 +0100

    Update follow.py

--- follow.py
+++ follow.py
@@ -61,12 +61,12 @@ def following():
     
     x=0
     n_users=0
-    for i in range(last_page,last_page+pages):
-        res = sesh.get("https://api.github.com/users?since=" + str(i)).json()
-        for user in res:
-            following.append(user['login'])
-            n_users+=1
-            #print(user['login'])
+    # for i in range(last_page,last_page+pages):
+    res = sesh.get("https://api.github.com/users?since=" + str(last_page)).json()
+    for user in res:
+        following.append(user['login'])
+        n_users+=1
+        #print(user['login'])
     f = open("last_page.txt", "w+")
     f.write(str(last_page+pages))
     print(n_users)
